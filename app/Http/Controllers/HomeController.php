7c4143eed83c7a63fcb6a6b5c6f121e4a528fb90
shun1121 <hikitanikun.ware80000@gmail.com>
2023-10-12 10:17:41 +0900

    create Memo model and store data in database

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Memo;
 
 class HomeController extends Controller
 {
@@ -25,10 +26,41 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
     public function create()
     {
         // ログインしているユーザ情報をViewに渡す
         $user = \Auth::user();
         return view('create', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        // フォームに入力されたメモの内容やユーザ情報を受け取ることができる。
+        $data = $request->all();
+        // dd($data);
+        // POSTされたデータをDB（memosテーブル）に挿入
+        // MEMOモデルにDBへ保存する命令を出す
+
+        // 同じタグがあるか確認
+        // $exist_tag = Tag::where('name', $data['tag'])->where('user_id', $data['user_id'])->first();
+        // if( empty($exist_tag['id']) ){
+        //     //先にタグをインサート
+        //     $tag_id = Tag::insertGetId(['name' => $data['tag'], 'user_id' => $data['user_id']]);
+        // } else {
+        //     $tag_id = $exist_tag['id'];
+        // }
+
+        //タグのIDが判明する
+        // タグIDをmemosテーブルに入れてあげる
+        $memo_id = Memo::insertGetId([
+            'content' => $data['content'],
+            'user_id' => $data['user_id'],
+            // 'tag_id' => $tag_id,
+            'status' => 1
+        ]);
+
+        // リダイレクト処理
+        return redirect()->route('home');
     }
 }
