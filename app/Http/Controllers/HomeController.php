9910aecf2fb15dc181ed942d0017a37a8a7a01b3
shun1121 <hikitanikun.ware80000@gmail.com>
2023-10-12 10:45:26 +0900

    get filtered memos in index method

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,7 +24,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user = \Auth::user();
+        $memos = Memo::where('user_id', $user['id'])->where('status', 1)->orderBy('updated_at', 'DESC')->get();
+        // dd($memos);
+        return view('home', compact('user', 'memos'));
     }
 
     public function create()
