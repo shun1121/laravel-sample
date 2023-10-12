9d92174ce220b16081688cb288bb6fe70ca6ab7b
shun1121 <hikitanikun.ware80000@gmail.com>
2023-10-12 10:57:37 +0900

    display memo content on top page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -92,6 +92,9 @@
                     <div class="card h-100">
                         <div class="card-header d-flex">メモ一覧 <a class='ml-auto' href='/create'><i class="fas fa-plus-circle"></i></a></div>
                         <div class="card-body p-2">
+                            @foreach($memos AS $memo)
+                                <p>{{ $memo['content'] }}</p>
+                            @endforeach
                         </div>
                     </div>
                 </div> <!-- col-md-3 -->
